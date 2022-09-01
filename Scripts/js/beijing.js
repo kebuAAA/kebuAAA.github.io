e6a9ef904dfe5a4efc74a06a8e65b1eb0f2b3582
kebuAAA <96560962+kebuAAA@users.noreply.github.com>
2022-09-01 17:17:38 +0800

    Site updated: 2022-09-01 17:17:36

--- Scripts/js/beijing.js
+++ Scripts/js/beijing.js
@@ -1,1 +1,57 @@
-window.onload=()=>{try{let e="",t=document.querySelector(".fj-gallery");t.addEventListener("contextmenu",(e=>{e.preventDefault()})),t.addEventListener("dragend",(e=>{changeBg("url("+e.target.src+")")})),t.addEventListener("touchstart",(t=>{e=setTimeout((()=>{changeBg("url("+t.target.src+")")}),500)})),t.addEventListener("touchend",(t=>{clearTimeout(e)}))}catch(e){}};try{s=localStorage.getItem("blogbg"),"null"!=s&&changeBg(s)}catch(e){}try{document.getElementById("color").addEventListener("change",(e=>{changeBg(e.path[0].value)}))}catch(e){}function changeBg(e){let t=document.getElementById("web_bg");"#"==e.charAt(0)?(t.style.backgroundColor=e,t.style.backgroundImage="none"):"null"==e?(localStorage.setItem("blogbg",null),location.reload()):t.style.backgroundImage=e,localStorage.setItem("blogbg",e)}
+window.onload = () => {
+    // 添加 相册壁纸拖拽（电脑）和长按（手机）切换背景 监听 没相册可删除，或者看下面创建相册教程
+    try {
+        let time = ''
+        let imgbox = document.querySelector('.fj-gallery')
+        imgbox.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+        })
+        imgbox.addEventListener('dragend', (e) => {
+            let bg = 'url(' + e.target.src + ')'
+            changeBg(bg)
+        })
+        imgbox.addEventListener('touchstart', (e) => {
+            time = setTimeout(() => {
+                let bg = 'url(' + e.target.src + ')'
+                changeBg(bg);
+            }, 500);
+        })
+        imgbox.addEventListener('touchend', (e) => {
+            clearTimeout(time);
+        })
+    } catch (error) { }
+}
+
+// 背景localstorage
+try {
+    s = localStorage.getItem('blogbg');
+    if (s != 'null') {
+        changeBg(s)
+    }
+} catch (error) { }
+
+
+// 添加 拾色器 监听
+try {
+    document.getElementById('color').addEventListener('change', (e) => {
+        changeBg(e.path[0].value);
+    })
+} catch (error) { }
+
+// 切换背景函数
+function changeBg(s) {
+    let bg = document.getElementById('web_bg')
+    // 纯色
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+        // 换回网站原来的背景
+    } else if (s == 'null') {
+        localStorage.setItem('blogbg', null);
+        location.reload()
+        // 渐变和图片
+    } else {
+        bg.style.backgroundImage = s
+    }
+    localStorage.setItem('blogbg', s);
+}
